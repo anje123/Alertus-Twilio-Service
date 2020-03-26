ba79c87a5f8fbbf6fcb3698dc85777163b6ebce9
femmy <femianjorin0@gmail.com>
2020-03-26 07:45:20 +0100

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -61,8 +61,8 @@ Route::post('/question/update/{id}',[ 'uses' => 'API\QuestionController@updateQu
 Route::delete('/question/deactivate/{id}',[ 'uses' => 'API\QuestionController@deactivateQuestion']);
 Route::patch('/question/activate/{id}',[ 'uses' => 'API\QuestionController@activateQuestion']);
 Route::get('/survey/{surveyId}/questions', ['uses' => 'API\QuestionController@getQuestionsBySurveyId']);
-Route::delete('/questions/deactivated', ['uses' => 'API\QuestionController@getDeactivatedQuestions']);
-Route::patch('/questions/activated', ['uses' => 'API\QuestionController@getActivatedQuestions']);
+Route::delete('/question/deactivated', ['uses' => 'API\QuestionController@getDeactivatedQuestions']);
+Route::patch('/question/activated', ['uses' => 'API\QuestionController@getActivatedQuestions']);
 
 
 Route::get('/responses/{last_object_id}', ['uses' => 'API\QuestionResponseController@getResponses']);
